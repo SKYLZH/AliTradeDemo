f1d6c137b2004a4edcb6a2e256972c3f757e3b75
LxqSillybear <lxqdevin@qq.com>
2017-07-11 14:59:36 +0800

    LxqSillybear
    阿里百川可运行demo

---
+++ app/src/main/java/com/alibaba/sdk/trade/demo/MainActivity.java
@@ -0,0 +1,15 @@
+package com.alibaba.sdk.trade.demo;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public
+class
+MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
